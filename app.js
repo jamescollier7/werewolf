e9d7165c5f7d52a7e4cbcd416c0fb71f01474acc
Michael Collier <mikec@kuali.co>
2016-01-01 18:19:22 -0700

    Add folder structure, fix resource links, use new icons.

--- app.js
+++ app.js
@@ -3,52 +3,76 @@
 var WEREWOLF = (function () {
     'use strict';
     var i;
-    
+
     function loadCard(cardname) {
-        var cardViewer = $(".cardViewer"),
-            viewerRow = $(".cardViewerRow"),
-            path = cardname + ".html";
-        
-        cardViewer.load(path, function (response, status, xhr) {
-            // onComplete here
-            viewerRow.show(150, function () {
-                $("html, body").animate({ scrollTop: $(".cardViewerRow").offset().top }, "slow");
-            });
-        });
-    }
-    
-    function loadHouse(housename) {
-        var houseViewer = $(".houseViewer"),
-            viewerRow = $(".houseViewerRow"),
-            path = housename + ".html";
-        
-        houseViewer.load(path, function (response, status, xhr) {
-            // onComplete here
-            viewerRow.show(150, function () {
-                $("html, body").animate({ scrollTop: $(".houseViewerRow").offset().top }, "slow");
-            });
-        });
-    }
-    
-    $(function () {
-        var cards = $(".card"),
-            houses = $(".house");
-    
-        cards.each(function (cardCount) {
-            var card = $(cards[cardCount]),
-                html = card[0];
-            card.on("click", function () {
-                loadCard(card.attr("data"));
-            });
-        });
-        
-        
-        houses.each(function (houseCount) {
-            var house = $(houses[houseCount]),
-                html = house[0];
-            house.on("click", function () {
-                loadHouse(house.attr("data"));
-            });
+      var cardViewer = $(".cardViewer"),
+      viewerRow = $(".cardViewerRow"),
+      path = "html-content/" + cardname + ".html";
+
+      cardViewer.load(path, function (response, status, xhr) {
+        // onComplete here
+        viewerRow.show(150, function () {
+            $("html, body").animate({ scrollTop: $(".cardViewerRow").offset().top }, "slow");
         });
     });
+    }
+
+    function loadHouse(housename) {
+      var houseViewer = $(".houseViewer"),
+      viewerRow = $(".houseViewerRow"),
+      path = "html-content/" + housename + ".html";
+
+      houseViewer.load(path, function (response, status, xhr) {
+          // onComplete here
+          viewerRow.show(150, function () {
+              $("html, body").animate({ scrollTop: $(".houseViewerRow").offset().top }, "slow");
+          });
+      });
+    }
+
+  $(document).ready(function(e) {
+      try {
+        var cardOptions = {
+          visibleRows: 4,
+          rowHeight:200,
+          on: {
+            change: function(data) {
+              var val = data.value;
+              if (val != "") {
+                loadCard(val);
+              }
+            }
+          }
+        };
+        var houseOptions = {
+          visibleRows: 4,
+          rowHeight:200,
+          on: {
+            change: function(data) {
+              var val = data.value;
+              if (val != "") {
+                loadHouse(val);
+              }
+            }
+          }
+        };
+        $("#cardMenu").msDropdown(cardOptions);
+        $("#houseMenu").msDropdown(houseOptions);
+      } catch(e) {
+          alert(e.message);
+      }
+  });
 }());
+
+function loadHouse(housename) {
+    var houseViewer = $(".houseViewer"),
+        viewerRow = $(".houseViewerRow"),
+        path = housename + ".html";
+
+    houseViewer.load(path, function (response, status, xhr) {
+        // onComplete here
+        viewerRow.show(150, function () {
+            $("html, body").animate({ scrollTop: $(".houseViewerRow").offset().top }, "slow");
+        });
+    });
+}
