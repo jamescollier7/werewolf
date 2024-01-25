4dd78317d970602b17f6657969c44e2aba35290c
James Collier <jamescollier7@users.noreply.github.com>
2024-01-25 09:49:01 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,48 +2,51 @@
     'use strict';
 
     const app
+    const data
 
     const init = async () => {
-        const data = await getRoles()
-        const dropdownHtml = await buildDropdownHtml(data)
         app = document.getElementById(`app`)
+        data = await getData()
+        const rolesHtml = await buildDropdownHtml(data.roles)
+        const housesHtml = await buildDropdownHtml(data.houses)
 
-        app.innerHTML += dropdownHtml
+        document.querySelector(`.dropdown-list.roles`).innerHTML += rolesHtml
+        document.querySelector(`.dropdown-list.houses`).innerHTML += housesHtml
 
         bindClickEvents()
     }
 
     const buildDropdownHtml = async (data) => {
-        const roleHtml = await data.roles.map(({name, title, altTitles, descriptionHtml}) => {
+        return await data.map(({name, title, altTitles, descriptionHtml}) => {
             return `<div id="${name}" class="dropdown-item"><img src="images/msdropdown/icons/roles/${name}.png"><h2>${title}</h2></div>`
         }).join(``)
-
-        return `<div class="dropdown">
-                    <button class="dropdown-button role" type="button">Choose Role</button>
-                    <div class="dropdown-list hidden">${roleHtml}</div>
-                </div>`
     }
 
-    const getRoles = async () => {
+    const getData = async () => {
         const response = await fetch(`https://jamescollier7.github.io/werewolf/src/data.json`)
         return await response.json()
     }
 
-    const selectRole = async () => {
-        app.innerHTML = `asdf`
-    }
-
     const bindClickEvents = () => {
-        document.addEventListener(`click`, (event) => {
-            const target = event.target
-            console.log(target)
-            if (target.classList.contains(`dropdown-button`)) {
-                const parent = target.parentElement
-                const dropdownList = parent.querySelector(`.dropdown-list`)
-                dropdownList.classList.toggle(`hidden`)
-            } else if (target.classList.contains(`dropdown-item`) {
-                
-            }
+        // open/close dropdown
+        const dropdownButtons = document.querySelectorAll(`.dropdown button.button`)
+        dropdownButtons.addEventListener(`click`, (event) => {
+            // which dropdown?
+            console.log(event.currentTarget)
+        })
+
+        // select item
+        const dropdownList = document.querySelectorAll(`.dropdown .dropdown-list`)
+        dropdownList.addEventListener(`click`, (event) => {
+            // which item
+            localStorage.setItem("myCat", "Tom")
+        })
+
+        // do reset
+        const resetButton = document.querySelector(`button.reset`)
+        dropdownButtons.addEventListener(`click`, (event) => {
+            localStorage.clear()
+            location.reload()
         })
     }
     document.readyState !== `loading` ? init() : document.addEventListener(`readystatechange`, () => {
